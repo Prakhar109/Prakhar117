70b91f39453c7276e0bec693c0f0955a1fcbe047
prakhar <prakharghatia@gmail.com>
2021-07-01 10:42:36 +0530

    hello India

--- MyApplication6/app/src/main/java/com/example/myapplication6/MainActivity.kt
+++ MyApplication6/app/src/main/java/com/example/myapplication6/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("HEll");
+        println("HELLO INDIA");
         setContentView(R.layout.activity_main)
     }
 }
